55e6e904e561afb1dd758326bf7aa6eb9732f74b
David Leclerc <david_leclerc@hotmail.ca>
2016-05-28 00:55:33 +0200

    Renamed file.

---
+++ MeinKPS/reporter.py
@@ -0,0 +1,56 @@
+#! /usr/bin/python
+
+
+
+"""
+================================================================================
+Title:    reporter
+Author:   David Leclerc
+Version:  0.1
+Date:     27.05.2016
+License:  GNU General Public License, Version 3
+          (http://www.gnu.org/licenses/gpl.html)
+Overview: ...
+Notes:    ...
+================================================================================
+"""
+
+
+
+# LIBRARIES
+import os
+import sys
+import time
+import numpy as np
+import json
+
+
+
+# DEFINITIONS
+NOW = datetime.datetime.now()
+
+
+
+class Reporter:
+
+    # BASAL RECORDER CHARACTERISTICS
+    TALKATIVE        = True
+
+
+
+def main():
+
+    """
+    ============================================================================
+    MAIN
+    ============================================================================
+    """
+
+    # Instanciate a reporter for me
+    reporter = Reporter()
+
+
+
+# Run this when script is called from terminal
+if __name__ == "__main__":
+    main()
