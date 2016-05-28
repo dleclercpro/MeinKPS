e795afd46986fe863dd448c33271b09ecbc22e81
David Leclerc <david_leclerc@hotmail.ca>
2016-05-28 18:51:07 +0200

    Modified date.

--- MeinKPS/pump.py
+++ MeinKPS/pump.py
@@ -7,7 +7,7 @@
 Title:    pump
 Author:   David Leclerc
 Version:  0.1
-Date:     27.05.2016
+Date:     28.05.2016
 License:  GNU General Public License, Version 3
           (http://www.gnu.org/licenses/gpl.html)
 Overview: This is a script that contains a handful of commands that can be sent
