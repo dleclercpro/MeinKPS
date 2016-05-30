ed0ae8eb51f09036f84c1cb06d2ccc5b91ec9201
David Leclerc <david_leclerc@hotmail.ca>
2016-05-30 23:48:01 +0200

    Working on the reporter.

--- MeinKPS/reporter.py
+++ MeinKPS/reporter.py
@@ -7,7 +7,7 @@
 Title:    reporter
 Author:   David Leclerc
 Version:  0.1
-Date:     27.05.2016
+Date:     30.05.2016
 License:  GNU General Public License, Version 3
           (http://www.gnu.org/licenses/gpl.html)
 Overview: ...
@@ -15,26 +15,146 @@ Notes:    ...
 ================================================================================
 """
 
+# TODO
+# - Create report and/or categories if non-existent
+
 
 
 # LIBRARIES
 import os
 import sys
 import time
+import datetime as datetime
 import numpy as np
 import json
 
 
 
-# DEFINITIONS
-NOW = datetime.datetime.now()
-
-
-
 class Reporter:
 
-    # BASAL RECORDER CHARACTERISTICS
-    TALKATIVE        = True
+    # REPORTER CHARACTERISTICS
+    TALKATIVE = True
+
+
+
+    def addEntry(self):
+
+        """
+        ========================================================================
+        ADDENTRY
+        ========================================================================
+        """
+
+
+
+    def printEntries(self):
+
+        """
+        ========================================================================
+        PRINTENTRIES
+        ========================================================================
+        """
+
+        # Load reports
+        with open("Reports/insulin.json", "r") as f:
+            insulin_report = json.load(f)
+
+        # Print report entries
+        print insulin_report
+
+
+
+    def addReservoirLevelEntry(self):
+
+        """
+        ========================================================================
+        ADDRESERVOIRLEVELENTRY
+        ========================================================================
+        """
+
+        # Load reports
+        with open("Reports/pump.json", "r") as f:
+            insulin_report = json.load(f)
+
+        # ...
+        for i in range(4):
+            time.sleep(1)
+
+            now = datetime.datetime.now()
+            now_str = datetime.datetime.strftime(now, "%Y.%m.%d - %H:%M:%S")
+
+            level = i * 4.5
+
+            insulin_report["Reservoir Levels"][now_str] = level
+
+        # Save to file
+        with open("Reports/pump.json", "w") as f:
+            json.dump(insulin_report, f,
+                      indent = 4, separators = (",", ": "), sort_keys = True)
+
+
+
+    def addBolusEntry(self):
+
+        """
+        ========================================================================
+        ADDBOLUSENTRY
+        ========================================================================
+        """
+
+        # Load reports
+        with open("Reports/insulin.json", "r") as f:
+            insulin_report = json.load(f)
+
+        # ...
+        for i in range(4):
+            time.sleep(1)
+
+            now = datetime.datetime.now()
+            now_str = datetime.datetime.strftime(now, "%Y.%m.%d - %H:%M:%S")
+
+            bolus = i * 5.0
+
+            insulin_report["Boluses"][now_str] = bolus
+
+        # Save to file
+        with open("Reports/insulin.json", "w") as f:
+            json.dump(insulin_report, f,
+                      indent = 4, separators = (",", ": "), sort_keys = True)
+
+
+
+    def addTemporaryBasalEntry(self):
+
+        """
+        ========================================================================
+        ADDTEMPORARYBASALENTRY
+        ========================================================================
+        """
+
+        # Load reports
+        with open("Reports/insulin.json", "r") as f:
+            insulin_report = json.load(f)
+
+        # ...
+        for i in range(4):
+            time.sleep(1)
+
+            now = datetime.datetime.now()
+            now_str = datetime.datetime.strftime(now, "%Y.%m.%d - %H:%M:%S")
+
+            rate = i * 1.5
+            units = "U/h"
+            duration = i * 30
+
+            insulin_report["Temporary Basals"][now_str] = [rate,
+                                                           units,
+                                                           duration]
+
+        # Save to file
+        with open("Reports/insulin.json", "w") as f:
+            json.dump(insulin_report, f,
+                      indent = 4, separators = (",", ": "), sort_keys = True)
 
 
 
@@ -48,6 +168,11 @@ def main():
 
     # Instanciate a reporter for me
     reporter = Reporter()
+
+    # Add entries to reports
+    reporter.addReservoirLevelEntry()
+    reporter.addBolusEntry()
+    reporter.addTemporaryBasalEntry()
 
 
 
