1e0747d760cf5aa0a77e9237c8a4a64016c723a3
David Leclerc <david_leclerc@hotmail.ca>
2017-07-11 23:27:13 +0200

    Test.

--- MeinKPS/Pump/pump.py
+++ MeinKPS/Pump/pump.py
@@ -1483,13 +1483,13 @@ def main():
     #pump.buttons.push("ESC")
     #pump.buttons.push("ACT")
     #pump.buttons.push("UP")
-    pump.buttons.push("DOWN")
+    #pump.buttons.push("DOWN")
 
     # Read pump battery level
-    pump.battery.read()
+    #pump.battery.read()
 
     # Read remaining amount of insulin in pump
-    pump.reservoir.read()
+    #pump.reservoir.read()
 
     # Read pump status
     #pump.status.read()
@@ -1502,34 +1502,34 @@ def main():
     #pump.settings.verify()
 
     # Read BG units set in pump's bolus wizard
-    pump.units["BG"].read()
+    #pump.units["BG"].read()
 
     # Read carb units set in pump's bolus wizard
-    pump.units["C"].read()
+    #pump.units["C"].read()
 
     # Read current TB units
-    pump.units["TB"].read()
+    #pump.units["TB"].read()
 
     # Set TB units
     #pump.units["TB"].set("U/h")
     #pump.units["TB"].set("%")
 
     # Read BG targets stored in pump
-    pump.BGTargets.read()
+    #pump.BGTargets.read()
 
     # Read insulin sensitivity factors stored in pump
-    pump.ISF.read()
+    #pump.ISF.read()
 
     # Read carb sensitivity factors stored in pump
-    pump.CSF.read()
+    #pump.CSF.read()
 
     # Read basal profile stored in pump
-    pump.basal.read("Standard")
+    #pump.basal.read("Standard")
     #pump.basal.read("A")
     #pump.basal.read("B")
 
     # Read daily totals on pump
-    pump.dailyTotals.read()
+    #pump.dailyTotals.read()
 
     # Read pump history
     #pump.history.read(2)
@@ -1538,7 +1538,7 @@ def main():
     #pump.bolus.deliver(0.6)
 
     # Read current TB
-    pump.TB.read()
+    #pump.TB.read()
 
     # Send TB to pump
     #pump.TB.set(0.3, "U/h", 30)
