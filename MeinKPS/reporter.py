011f482d139c9002e6f44859e2d344f0b669e2c9
David Leclerc <david_leclerc@hotmail.ca>
2017-08-28 22:46:47 +0200

    New data.

--- MeinKPS/reporter.py
+++ MeinKPS/reporter.py
@@ -707,8 +707,8 @@ class Path:
         """
 
         # Merge path
-        #return os.sep + os.sep.join(self.list)
-        return os.sep.join(self.list)
+        return os.sep + os.sep.join(self.list)
+        #return os.sep.join(self.list)
 
 
 
