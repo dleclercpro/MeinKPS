df96e6f4dd3e0e3404af57d8c18032f06907aa05
David Leclerc <david_leclerc@hotmail.ca>
2019-07-29 18:51:12 -0400

    "

---
+++ Tests/test_calculator.py
@@ -0,0 +1,27 @@
+#! /usr/bin/python
+# -*- coding: utf-8 -*-
+
+"""
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    Title:    test_calculator
+
+    Author:   David Leclerc
+
+    Version:  0.1
+
+    Date:     29.07.2019
+
+    License:  GNU General Public License, Version 3
+              (http://www.gnu.org/licenses/gpl.html)
+
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+"""
+
+# LIBRARIES
+import pytest
+
+
+
+# USER LIBRARIES
+import calculator
